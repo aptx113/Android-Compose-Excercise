d31aaee21d6b29eab0a91174e771aecd14257bf1
George <aptx113@gmail.com>
2023-02-09 14:01:51 +0800

    Add inventory db

--- app/src/main/java/com/danteyu/android_compose_exercise/features/inventory/data/db/Item.kt
+++ app/src/main/java/com/danteyu/android_compose_exercise/features/inventory/data/db/Item.kt
@@ -1,9 +1,13 @@
 package com.danteyu.android_compose_exercise.features.inventory.data.db
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.danteyu.android_compose_exercise.features.inventory.ui.item.ItemUiState
 
+@Entity(tableName = "items")
 data class Item(
-    val id: Int,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val name: String,
     val price: Double,
     val quantity: Int
